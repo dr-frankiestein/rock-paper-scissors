ef5c837c883593ead517f7d81d4c4fae4b55fc9c
dr-frankiestein <150306010+dr-frankiestein@users.noreply.github.com>
2023-12-06 21:21:00 -0500

    Write function to play one round of game

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // use integer to choose element from array
 
 function getComputerChoice() {
-    const choices = ['rock', 'paper', 'scissors'];
+    const choices = ['Rock', 'Paper', 'Scissors'];
     // let num = oneTwoThree();
     // console.log(num); // for debugging
     // return choices[num];
@@ -15,3 +15,43 @@ function oneTwoThree() {
     return Math.floor(Math.random() * 3);
 }
 
+// write a function that plays a round of RPS
+// takes two parameters playerSelection & computerSelection
+// returns string declaring winner of round: "You Lose! Paper beats Rock"
+
+function playRound(playerSelection, computerSelection) {
+
+    playerSelection = capitalizeChoice(playerSelection);
+    
+    if ((playerSelection === 'Rock' && computerSelection === 'Paper')
+      || (playerSelection === 'Paper' && computerSelection === 'Scissors')
+      || (playerSelection === 'Scissors' && computerSelection === 'Rock')) {
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    } else if ((computerSelection === 'Rock' && playerSelection === 'Paper')
+      || (computerSelection === 'Paper' && playerSelection === 'Scissors')
+      || (computerSelection === 'Scissors' && playerSelection === 'Rock')) {
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
+    } else if (computerSelection === playerSelection){
+        return 'Tie! Replay the round';
+    };
+}
+
+
+
+ function capitalizeChoice(playerSelection) {
+
+    let lowSelection = playerSelection.toLowerCase();
+    let firstLetter = lowSelection.charAt(0).toUpperCase();
+    capitalizedSelection = firstLetter + lowSelection.slice(1);
+    // console.log(capitalizedSelection);
+
+    return capitalizedSelection;
+ }
+
+ let playerSelection = 'paper'; 
+ const computerSelection = getComputerChoice();
+
+ console.log('You: ' + capitalizeChoice(playerSelection));
+ console.log('Computer: ' + computerSelection);
+
+ console.log(playRound(playerSelection, computerSelection));
