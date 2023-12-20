7e1c48e71c3921be125571212331933a89745248
dr-frankiestein <150306010+dr-frankiestein@users.noreply.github.com>
2023-12-20 18:03:35 -0500

    Write new functions and edit old for full gameplay.
    Old single round function was modified to accurately count the rounds
    played and calculate the score at the end. Game played to best of 5,
    but ties are not counted as a point or as a round.

--- script.js
+++ script.js
@@ -1,57 +1,83 @@
-// create array with ['rock', 'paper', 'scissors']
-// function chooses integer 0, 1, or 2
-// use integer to choose element from array
+
+function game() {
+    let score = 0;
+    let count = 0;
+
+    while (count<5) {
+
+        showStartMessage(count, score);
+        let playerSelection = prompt("Rock, Paper, or Scissors?");
+        playerSelection = capitalizeChoice(playerSelection);
+        let computerSelection = getComputerChoice();
+        let result = playRound(playerSelection, computerSelection);
+
+        if (result === true) {
+            console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+            score++;
+            count++;
+        } else if (result === false) {
+            console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+            // score--;
+            count++;
+        } else if (result === null) {
+            console.log('Tie! Replay the round');
+        } else {
+            console.log('Not an option. Try again: Rock, Paper, or Scissors?');
+        }
+    }
+
+    showWinner(count, score);    
+ }
 
 function getComputerChoice() {
     const choices = ['Rock', 'Paper', 'Scissors'];
-    // let num = oneTwoThree();
-    // console.log(num); // for debugging
-    // return choices[num];
     return choices[oneTwoThree()];
 }
 
-// could rewrite this inside the other fxn as a anonymous or arrow fxn
 function oneTwoThree() {
     return Math.floor(Math.random() * 3);
 }
 
-// write a function that plays a round of RPS
-// takes two parameters playerSelection & computerSelection
-// returns string declaring winner of round: "You Lose! Paper beats Rock"
 
 function playRound(playerSelection, computerSelection) {
-
     playerSelection = capitalizeChoice(playerSelection);
     
-    if ((playerSelection === 'Rock' && computerSelection === 'Paper')
-      || (playerSelection === 'Paper' && computerSelection === 'Scissors')
-      || (playerSelection === 'Scissors' && computerSelection === 'Rock')) {
-        return `You Lose! ${computerSelection} beats ${playerSelection}`;
-    } else if ((computerSelection === 'Rock' && playerSelection === 'Paper')
-      || (computerSelection === 'Paper' && playerSelection === 'Scissors')
-      || (computerSelection === 'Scissors' && playerSelection === 'Rock')) {
-        return `You Win! ${playerSelection} beats ${computerSelection}`;
+    if ((playerSelection === 'Rock' && computerSelection === 'Paper') ||
+      (playerSelection === 'Paper' && computerSelection === 'Scissors') ||
+      (playerSelection === 'Scissors' && computerSelection === 'Rock')) {
+        return false;
+    } else if ((computerSelection === 'Rock' && playerSelection === 'Paper') ||
+      (computerSelection === 'Paper' && playerSelection === 'Scissors') ||
+      (computerSelection === 'Scissors' && playerSelection === 'Rock')) {
+        return true;
     } else if (computerSelection === playerSelection){
-        return 'Tie! Replay the round';
-    };
+        return null;
+    } else {
+        return NaN;
+    }
 }
 
 
-
  function capitalizeChoice(playerSelection) {
-
     let lowSelection = playerSelection.toLowerCase();
     let firstLetter = lowSelection.charAt(0).toUpperCase();
     capitalizedSelection = firstLetter + lowSelection.slice(1);
-    // console.log(capitalizedSelection);
-
     return capitalizedSelection;
  }
 
- let playerSelection = 'paper'; 
- const computerSelection = getComputerChoice();
 
- console.log('You: ' + capitalizeChoice(playerSelection));
- console.log('Computer: ' + computerSelection);
+ function showStartMessage(count, score) {
+    if (count === 0) {
+        console.log('New Game! Best out of 5. \n Round 1')
+    } else {
+        console.log(`Round ${count+1} \n Score: You ${score} vs Computer ${count-score}`);
+    }
+ }
 
- console.log(playRound(playerSelection, computerSelection));
+ function showWinner(count, score) {
+    if (score >= 3) {
+        console.log(`Game Over! \n You Win! You: ${score} vs Computer: ${count-score}`);
+    } else {
+        console.log(`Game Over! \n You Lose! You: ${score} vs Computer: ${count-score}`)
+    }
+ }
